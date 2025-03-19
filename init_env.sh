3626135930193850a4c83aeb672d820cda9b4d78
FlagerLee <46860743+FlagerLee@users.noreply.github.com>
2025-03-19 14:28:46 +0800

    Update init_env.sh

--- init_env.sh
+++ init_env.sh
@@ -1,3 +1,3 @@
 export OPENAI_BASE_URL=https://api.ai.cs.ac.cn/v1
-export OPENAI_API_KEY=sk-NqJSHi4mAwoiOUm4SRTsxqnrD9vJya5wkIBv1lv7jREczTXV
+export OPENAI_API_KEY=sk-xxx
 export LLM_MODEL=gpt-4o-mini
